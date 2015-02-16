4fc49b4a25975486c1cd308e8008aece45b9c985
Yornie Westink <jinxie88@live.nl>
2015-02-16 10:58:19 +0100

    MenuChanges + AudioManager(WIP)

---
+++ Assets/Scripts/AudioList.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AudioList : MonoBehaviour 
+{
+	//Menu
+	public AudioClip clicksound;
+	public AudioClip menuMusic;
+
+	//EffectSounds
+	public AudioClip hitSound;
+	public AudioClip walkSound;
+
+
+	public void Lists()
+	{
+		List<AudioClip> effectSound = new List<AudioClip> ();
+
+		effectSound.Add (clicksound);
+		effectSound.Add (menuMusic);
+		effectSound.Add (hitSound);
+		effectSound.Add (walkSound);
+
+		foreach (AudioClip effectS in effectSound) 
+		{
+
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/UI/Menu/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour 
+{
+	//Menu
+	
+	public AudioClip MenuMusic;
+	public AudioClip ClickSound;
+	
+	//BackGround Music
+	private AudioClip BackGroundMusic1;
+	
+
+
+
+}
+
