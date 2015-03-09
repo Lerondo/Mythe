6c395a2ec1bb8596d46fadd15068e9b1cc01f8b6
Yornie Westink <jinxie88@live.nl>
2015-03-09 15:54:50 +0100

    Wind + Art inplementation

--- Assets/Scripts/Controller/WindController.cs
+++ Assets/Scripts/Controller/WindController.cs
@@ -2,62 +2,75 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class WindController : MonoBehaviour {
+public class WindController : MonoBehaviour 
+{
 	float factor = 5.0f;
 	
 	private float startTime;
 	private Vector3 startPos;
-
+	
 	void Update()
 	{
 		if(Input.GetMouseButtonDown(0))
 		{
-		
 			OnMouseDown();
 		} else if(Input.GetMouseButtonUp(0))
 		{
 			OnMouseUp();
 		}
 	}
-
+	
 	List<Rigidbody> rigidbodies = new List<Rigidbody>();
-
-
+	
+	
 	void OnMouseDown() 
 	{
-		//clear list
-		rigidbodies.Clear ();
-
+		/*rigidbodies.Clear ();
+		
 		startTime = Time.time;
-		startPos = Input.mousePosition;
-		startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		startPos.z = transform.position.z - Camera.main.transform.position.z;
+		startPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		startPos.z = 0;
 
-		//Draw collider & add rigidbodies inside the drawn collider to the list
-		Vector3 spherePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		spherePos.z = 0;
-		Collider[] touchedRigidbodies = Physics.OverlapSphere (spherePos, 0.5f);
-
-		foreach(Collider col in touchedRigidbodies)
+		Debug.Log (startPos+"StartPos");
+		
+		Collider[] touchedRigidbodies = Physics.OverlapSphere (startPos, 0.000005f);
+		
+		foreach (Collider col in touchedRigidbodies) 
 		{
 			if(col.rigidbody)
 			{
-				rigidbodies.Add(col.rigidbody);
+				rigidbodies.Add (col.rigidbody);
 			}
-		}
-	
-	void OnMouseUp() {
+		}*/
+
+		rigidbodies.Clear ();
+
+		startTime = startTime.time;
+		startPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		startPos.z = 0;
+
+
+
+
+	}
+
+	void OnMouseUp() 
+	{
 		Vector3 endPos = Input.mousePosition;
-		endPos.y = transform.position.y - Camera.main.transform.position.y;
+		endPos.z = 0;
 		endPos = Camera.main.ScreenToWorldPoint(endPos);
 
-		Vector3 force = endPos - startPos;
-		force.y = force.magnitude;
+		Debug.Log (endPos+"Endpos");
+		
+		
+		Vector3 force =  endPos - startPos;
+		force.z = 5f;
 		force /= (Time.time - startTime);
-
-
+		
+		
 		foreach(Rigidbody rbody in rigidbodies)
 		{
+			Debug.Log(rbody.name);
 			rbody.AddForce(force * factor);
 		}
 	}
