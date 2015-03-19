2a25602219e52cee5d3433d98017ce0fd8463269
Yornie Westink <jinxie88@live.nl>
2015-03-19 10:08:29 +0100

    Audio + new enemy

--- Assets/Scripts/AudioList.cs
+++ Assets/Scripts/AudioList.cs
@@ -6,30 +6,62 @@ using System.Collections.Generic;
 public class AudioList : MonoBehaviour 
 {
 	//Music
+	[SerializeField]private AudioClip preMusic;
 	[SerializeField]private AudioClip menuMusic;
 	[SerializeField]private AudioClip hubmusic;
 
-	//EffectSounds
-	[SerializeField]private AudioClip hitSound;
+	//PlayerSounds
 	[SerializeField]private AudioClip walkSound;
+	[SerializeField]private AudioClip climbSound;
+	[SerializeField]private AudioClip jumpSound;
+	[SerializeField]private AudioClip playerHitSound;
+	[SerializeField]private AudioClip attackingSound;
 
+	//ObjectSounds
+	[SerializeField]private AudioClip arrowShotSound;
+	[SerializeField]private AudioClip arrowInpactSound;
+	[SerializeField]private AudioClip hitSound;
+	[SerializeField]private AudioClip chargeShout;
 
+	//WorldSounds
+	[SerializeField]private AudioClip birdSound;
+	[SerializeField]private AudioClip windSound;
+
+	private List<AudioClip> AudioL = new List<AudioClip> ();
 
 	public void Lists()
 	{
-		List<AudioClip> AudioL = new List<AudioClip> ();
 		//Music
+		AudioL.Add (preMusic);
 		AudioL.Add (menuMusic);
 		AudioL.Add (hubmusic);
-
-		//Effects
-		AudioL.Add (hitSound);
+	
+		//PlayerSounds
 		AudioL.Add (walkSound);
+		AudioL.Add (climbSound);
+		AudioL.Add (jumpSound);
+		AudioL.Add (playerHitSound);
+		AudioL.Add (attackingSound);
+		AudioL.Add (chargeShout);
 
-		foreach (AudioClip effectS in AudioL) 
+		//ObjectSound
+		AudioL.Add (arrowShotSound);
+		AudioL.Add (arrowInpactSound);
+		AudioL.Add (hitSound);
+
+		//WorldSounds
+		AudioL.Add (birdSound);
+		AudioL.Add (windSound);
+
+		foreach (AudioClip SoundF in AudioL) 
 		{
 
 		}
+	}
+
+	void Awake()
+	{
+		Lists ();
 	}
 
 
