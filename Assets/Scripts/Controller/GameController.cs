c0053843fd19e698d5c428bc0b5b06d1de26917d
Yornie Westink <jinxie88@live.nl>
2015-03-27 01:39:36 +0100

    Audio

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -7,10 +7,7 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		_audioSource = GetComponent<AudioSource> ();
 
-		_audioSource.clip = GameObject.FindGameObjectWithTag(Tags.GameController).GetComponent<AudioList>().PlayAudio("MainMusic");
-		_audioSource.Play();
 
 		Physics.gravity = new Vector3(0,-15,0);
 	}
